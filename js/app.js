1302ad402d5301311a2b17b846924cd80131ce03
Ricardo Fernandez <v-rifern@microsoft.com>
2018-06-10 11:28:48 -0700

    enemy class created. Player class in progress.

--- js/app.js
+++ js/app.js
@@ -1,39 +1,55 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+class Enemy {
+  constructor(x,y,speed) {
+    // Variables applied to each of our instances go here,***
+    // we've provided one for you to get started***
+  this.x = x;
+  this.y = y;
+  this.speed = speed;
+    // The image/sprite for our enemies, this uses***
+    // a helper we've provided to easily load images***
+  this.sprite = 'images/enemy-bug.png';
+  }
+  // // Update the enemy's position, required method for game***
+  // // Parameter: dt, a time delta between ticks***
+  // Enemy.prototype.update = function(dt) {***
+       // You should multiply any movement by the dt parameter***
+       // which will ensure the game runs at the same speed for***
+       // all computers.***
+  update(dt) {
+    this.x += this.speed * dt;
+  }
+  // Draw the enemy on the screen, required method for game***
+  render(){
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+
+// Now write your own player class***
+// This class requires an update(), render() and***
+// a handleInput() method.***
+class Player {
+  constructor(x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.sprite = 'images/char-boy.png';
+  }
+}
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// Now instantiate your objects.***
+// Place all enemy objects in an array called allEnemies***
+// Place the player object in a variable called player ***
+let allEnemies = [];
+let player = new Player(200,380,0);
+// need to add function to create more than one enemy
+let enemy = new Enemy(0, 60, 100);
+allEnemies.push(enemy);
 
-
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// This listens for key presses and sends the keys to your ***
+// Player.handleInput() method. You don't need to modify this.***
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
