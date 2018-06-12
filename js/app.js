5cacd91dba59cd6d8a7b6da7b10281a270b06b34
Ricardo Fernandez <v-rifern@microsoft.com>
2018-06-12 09:16:49 -0700

    code organization changes/clean up

--- js/app.js
+++ js/app.js
@@ -1,14 +1,16 @@
+
+'use strict';
 // Enemies our player must avoid
 class Enemy {
   constructor(x,y,speed) {
     // Variables applied to each of our instances go here,***
     // we've provided one for you to get started***
-  this.x = x;
-  this.y = y;
-  this.speed = speed;
-    // The image/sprite for our enemies, this uses***
-    // a helper we've provided to easily load images***
-  this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+      // The image/sprite for our enemies, this uses***
+      // a helper we've provided to easily load images***
+    this.sprite = 'images/enemy-bug.png';
   }
   // // Update the enemy's position, required method for game***
   // // Parameter: dt, a time delta between ticks***
